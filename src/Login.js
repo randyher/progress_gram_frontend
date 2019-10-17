127e6763f5faf9bf499a2e9760f3a66f2df1057f
github username <randyherasme6@gmail.com>
2019-10-17 12:03:39 -0400

    login page

---
+++ src/Login.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+class Login extends React.Component {
+  render() {
+    return (
+      <div>
+          
+        <input type="text" placeholder="Username" />
+        <input type="password" placeholder="Password" />
+        
+      </div>
+    );
+  }
+}
+
+
+
+export default Login;
